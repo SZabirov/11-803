2ed1ddad201d184aacc5343a850a02addd4718d0
Salavat Zabirov <zab.sal42@gmail.com>
2019-02-15 16:24:41 +0300

    Homework task on maps added

---
+++ ClassProjects/Lesson03/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String s1 = "abc";
+	    String s2 = "abcd";
+        System.out.println(s1.compareTo(s2));
+    }
+}
